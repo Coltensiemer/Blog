7cca1aa67f55b9dbe750f1a836ac34f3f52b9d7a
Colten Siemergit <colten50@hotmail.com>
2022-12-25 13:52:49 -0600

    work on nav menu UI and JS functions for nav menu

---
+++ scripts/menu-toggle.js
@@ -0,0 +1,17 @@
+
+const menuBtnClose = document.getElementById('menu-btn-close'); 
+con
+const menuBtnOpen = document.getElementById('menu-btn'); 
+
+const navUI = document.querySelector("nav"); 
+
+
+
+
+// EVENT Listener 
+menuBtnOpen.addEventListener('click', (e)=> { 
+    e.preventDefault(); 
+
+    navUI.classList.remove('')
+
+} )
