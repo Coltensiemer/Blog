3675908bf169822b0022473ae7078b09b50d3f40
Colten Siemergit <colten50@hotmail.com>
2022-12-27 19:51:32 -0600

    added

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) { 
 
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./scripts")
+    eleventyConfig.addPassthroughCopy("//src/css");
+    eleventyConfig.addPassthroughCopy("./scripts");
+    eleventyConfig.addPassthroughCopy("./index.html");
     
     return { 
         dir: { 
